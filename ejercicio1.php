a33590d2090fd63f270adca07121248260b7a979
Mariano Rivero <marianorivero97@gmail.com>
2021-04-10 16:19:03 -0300

    Commit de ejercicios

---
+++ ejercicio1.php
@@ -0,0 +1,7 @@
+<?php
+	$v1 = 5;
+	$v2 = 5;
+	$res = $v1 + $v2;
+	
+	echo "El resultado es: ".$res;
+?>
---
+++ ejercicio4.php
@@ -0,0 +1,12 @@
+<?php
+	$v1 = 5;
+	$v2 = 10;
+
+	if ($v1 > $v2) {
+		echo "En numero mayor es: ".$v1;
+	}
+	else{
+		echo "En numero mayor es: ".$v2;
+	}
+
+?>
